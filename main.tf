64b7c0da72acf1758c7dd0c9b39688ab93dd623f
Srinivas P <spabballa@gmail.com>
2023-01-04 01:00:47 +0530

    deleted resources from remote

--- main.tf
+++ main.tf
@@ -1,23 +1,23 @@
-resource "azurerm_resource_group" "tf-vmrsgrp" {
-  name     = "vmrsgrp"
-  location = "West Europe"
-   tags = {
-    "environment" = var.environment
-  }
-}
+# resource "azurerm_resource_group" "tf-vmrsgrp" {
+#   name     = "vmrsgrp"
+#   location = "West Europe"
+#    tags = {
+#     "environment" = var.environment
+#   }
+# }
 
-resource "azurerm_resource_group" "tf-vnetrsgrp" {
-  name     = "vnetrsgrp"
-  location = "West Europe"
-   tags = {
-    "environment" = var.environment
-  }
-}
+# resource "azurerm_resource_group" "tf-vnetrsgrp" {
+#   name     = "vnetrsgrp"
+#   location = "West Europe"
+#    tags = {
+#     "environment" = var.environment
+#   }
+# }
 
-resource "azurerm_resource_group" "tf-manualrg" {
-  name     = "manualrg"
-  location = "eastus"
-  tags = {
-    "environment" = var.environment
-  }
-}
+# resource "azurerm_resource_group" "tf-manualrg" {
+#   name     = "manualrg"
+#   location = "eastus"
+#   tags = {
+#     "environment" = var.environment
+#   }
+# }
--- vnet.tf
+++ vnet.tf
@@ -1,25 +1,25 @@
 
 
-resource "azurerm_virtual_network" "tf-vnet01" {
-  name     = "tf-vnet"
-  resource_group_name = azurerm_resource_group.tf-vnetrsgrp.name 
-  location = azurerm_resource_group.tf-vnetrsgrp.location
-  address_space = ["10.1.0.0/16", "10.2.0.0/24"]
-   tags = {
-    "environment" = var.environment
-  }
-}
+# resource "azurerm_virtual_network" "tf-vnet01" {
+#   name     = "tf-vnet"
+#   resource_group_name = azurerm_resource_group.tf-vnetrsgrp.name 
+#   location = azurerm_resource_group.tf-vnetrsgrp.location
+#   address_space = ["10.1.0.0/16", "10.2.0.0/24"]
+#    tags = {
+#     "environment" = var.environment
+#   }
+# }
 
-resource "azurerm_subnet" "tf-SubnetA" {
-  name = "SubnetA"
-  virtual_network_name = azurerm_virtual_network.tf-vnet01.name
-  resource_group_name = azurerm_resource_group.tf-vnetrsgrp.name
-  address_prefixes = [ "10.1.0.0/28" ]
-}
+# resource "azurerm_subnet" "tf-SubnetA" {
+#   name = "SubnetA"
+#   virtual_network_name = azurerm_virtual_network.tf-vnet01.name
+#   resource_group_name = azurerm_resource_group.tf-vnetrsgrp.name
+#   address_prefixes = [ "10.1.0.0/28" ]
+# }
 
-resource "azurerm_subnet" "tf-SubnetB" {
-  name = "SubnetB"
-  virtual_network_name = azurerm_virtual_network.tf-vnet01.name
-  resource_group_name = azurerm_resource_group.tf-vnetrsgrp.name
-  address_prefixes = [ "10.1.0.16/28" ]
-}
+# resource "azurerm_subnet" "tf-SubnetB" {
+#   name = "SubnetB"
+#   virtual_network_name = azurerm_virtual_network.tf-vnet01.name
+#   resource_group_name = azurerm_resource_group.tf-vnetrsgrp.name
+#   address_prefixes = [ "10.1.0.16/28" ]
+# }
